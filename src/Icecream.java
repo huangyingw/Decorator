a7a17ff2b3ef15e366c2e3d0d46751fac1d10606
huangyingw@linux_programming <huangyingw@gmail.com>
2013-10-28 11:07:52 -0700

    n

---
+++ src/Icecream.java
@@ -0,0 +1,5 @@
+//http://javapapers.com/design-patterns/decorator-pattern/
+
+public interface Icecream {
+	public String makeIcecream();
+}
---
+++ src/NuttyDecorator.java
@@ -0,0 +1,15 @@
+
+public class NuttyDecorator extends IcecreamDecorator {
+
+	public NuttyDecorator(Icecream specialIcecream) {
+		super(specialIcecream);
+	}
+
+	public String makeIcecream() {
+		return specialIcecream.makeIcecream() + addNuts();
+	}
+
+	private String addNuts() {
+		return " + cruncy nuts";
+	}
+}
